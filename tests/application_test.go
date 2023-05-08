3dd6efb04bf24098ebe4d4022eb49a510d40b6f4
Lars Morten Bek <lars@beksoft.dk>
2023-05-08 05:18:07 +0200

    Added test for application.go * Added tests for start and shutdown application, the tests expect that the functions are not implemented yet

---
+++ tests/application_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"application"
+	"errors"
+	"helpers"
+	"testing"
+)
+
+func TestApplication_Start(test *testing.T) {
+	// maybe we will remove this - as start is not exported
+	test.Run("Testing Application Start", func(test *testing.T) {
+		app := new(application.Application)
+		result := app.Start().Error()
+		expected := errors.New("not implemented").Error()
+		helpers.StandardTestChecking(test, result, expected)
+	})
+}
+
+func TestApplication_Shutdown(test *testing.T) {
+	test.Run("Testing Application Shutdown", func(test *testing.T) {
+		app := new(application.Application)
+		result := app.Start().Error()
+		expected := errors.New("not implemented").Error()
+		helpers.StandardTestChecking(test, result, expected)
+	})
+}
