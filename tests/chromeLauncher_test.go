c8ece48d68731623999f2299290f50bcec280318
Lars Morten Bek <lars@beksoft.dk>
2023-05-09 00:07:58 +0200

    Fixed chromeLauncher_test.go * Fixed tests * Cleaned code * Nearly 100% code coverage added

--- tests/chromeLauncher_test.go
+++ tests/chromeLauncher_test.go
@@ -5,16 +5,17 @@ import (
 	"errors"
 	"fmt"
 	"helpers"
+	"strings"
 	"sync"
 	"testing"
 	"time"
 )
 
-func TestChromeLauncher_Start(test *testing.T) {
+func TestChromeLauncher_Installed(test *testing.T) {
 	test.Run("Testing ChromeLauncher Start", func(test *testing.T) {
 		app := new(application.ChromeLauncher)
 		waitgroup := &sync.WaitGroup{}
-
+		waitgroup.Add(1)
 		go func() {
 			// shutdown app after 3 seconds
 			time.Sleep(time.Second * 3)
@@ -26,7 +27,35 @@ func TestChromeLauncher_Start(test *testing.T) {
 			waitgroup.Done()
 		}()
 
+		result := app.Start()
+		if result != nil {
+			if strings.Contains(result.Error(), "file does not exist") && strings.Contains(result.Error(), "exec") {
+				helpers.PrintGotFatalError(test, "Could not start chrome, is it installed?")
+			}
+		}
+
+		expected := error(nil)
+		helpers.StandardTestChecking(test, result, expected)
+		waitgroup.Wait()
+	})
+}
+
+func TestChromeLauncher_Start(test *testing.T) {
+	test.Run("Testing ChromeLauncher Start", func(test *testing.T) {
+		app := new(application.ChromeLauncher)
+		waitgroup := &sync.WaitGroup{}
 		waitgroup.Add(1)
+		go func() {
+			// shutdown app after 3 seconds
+			time.Sleep(time.Second * 3)
+			err := app.Shutdown()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			waitgroup.Done()
+		}()
+
 		result := app.Start()
 		expected := error(nil)
 		helpers.StandardTestChecking(test, result, expected)
@@ -38,7 +67,7 @@ func TestChromeLauncher_Shutdown(test *testing.T) {
 	test.Run("Testing ChromeLauncher Shutdown", func(test *testing.T) {
 		app := new(application.ChromeLauncher)
 		waitgroup := &sync.WaitGroup{}
-		done := make(chan struct{})
+		waitgroup.Add(1)
 
 		go func() {
 			// shutdown app after 3 seconds
@@ -47,13 +76,7 @@ func TestChromeLauncher_Shutdown(test *testing.T) {
 			if result != nil {
 				fmt.Println(result)
 			}
-			close(done)
-		}()
-
-		waitgroup.Add(1)
-		go func() {
-			defer waitgroup.Done()
-			<-done
+			waitgroup.Done()
 		}()
 
 		result := app.Start()
@@ -64,16 +87,17 @@ func TestChromeLauncher_Shutdown(test *testing.T) {
 
 	test.Run("Testing ChromeLauncher Shutdown Without Starting It First", func(test *testing.T) {
 		app := new(application.ChromeLauncher)
-		wg := &sync.WaitGroup{}
+		waitgroup := &sync.WaitGroup{}
 
-		wg.Add(1)
+		waitgroup.Add(1)
 		go func() {
-			defer wg.Done()
+			time.Sleep(time.Second * 2)
+			defer waitgroup.Done()
 		}()
 
 		result := app.Shutdown()
 		expected := errors.New("chrome process is not running")
 		helpers.StandardTestChecking(test, result.Error(), expected.Error())
-		wg.Wait()
+		waitgroup.Wait()
 	})
 }
